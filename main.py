93846ad99e8d2ea6a93d4de069315c2bb3580aa4
Rachel <rachelburns7@gmail.com>
2019-07-26 14:53:59 -0500

    added styling

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Thisisasecret!'
-
-class LoginForm(FlaskForm):
-    username = StringField('Username')
-    password = PasswordField('Password')
+app.config['SECRET_KEY'] = 'f35ce59fdef81e127fd51e37008d4e13'
 
 @app.route('/')
+@app.route('/home')
 def hello():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def form():
-    form = LoginForm()
+    return render_template('login.html')
 
-    if form.validate_on_submit():
-        return 'The form has been submitted! The username is {}. The password is {}.'.format(form.username.data, form.password.data)
-    return render_template('form.html', form=form)
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
