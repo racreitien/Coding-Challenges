75fa721b3d5ff5b09b74478a84a8f2b2eba561d6
Rachel <rachelburns7@gmail.com>
2019-08-20 16:46:09 -0500

    Updated Login and Register pages

--- main.py
+++ main.py
@@ -9,14 +9,20 @@ app.config['SECRET_KEY'] = '19sdfk130jar2380ha08hfoun1094jo'
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Welcome back, {}!'.format(form.username.data))
+        return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form=RegistrationForm()
+    if form.validate_on_submit():
+        flash('Welcome to our website, {}!'.format(form.username.data))
+        return redirect(url_for('home'))
     return render_template('register.html', form=form)
 
 if __name__ == '__main__':
